5c684668fb4dd8f3d3bac7a04422c8c5c3f7d3df
Nonoroazoro <xiaochao.k@gmail.com>
2021-10-15 15:48:45 +0800

    upgrade packages

---
+++ .eslintrc.js
@@ -0,0 +1,21 @@
+const { isProd } = require("./scripts/build.config");
+
+const config = {
+    "extends": [
+        "eslint-config-zoro/eslint",
+        "eslint-config-zoro/typescript"
+    ],
+    "parserOptions": {
+        "project": "./tsconfig.eslint.json"
+    }
+};
+
+if (!isProd)
+{
+    config.rules = {
+        "no-console": "off",
+        "@typescript-eslint/no-unused-vars": "off"
+    };
+}
+
+module.exports = config;
